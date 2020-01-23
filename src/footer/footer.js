c4e655f00b1cdada8bc82ded8bcea5cdb135223f
juliaslohman <juliaslohman@gmail.com>
2020-01-23 14:10:17 -0800

    organization and deletions

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -14,14 +14,14 @@ class Footer extends React.Component {
 		return (
 			<footer>
 
-                <div className="allfooterimages">
+                <div className="all-footer-images">
                     <img src={cta} alt="California Taekwondo Association" width="60" height="60"/>
                     <img src={usat} alt="USA Taekwondo" width="131" height="60"/>
                     <img src={aau} alt="USA Amateur Athletic Union" width="51" height="60"/>
                     <img src={wtf} alt="World Taekwondo Federation" width="131" height="60"/>
                 </div>
 
-                <div className="footercontact">
+                <div className="footer-text">
                     LeeJon Taekwondo
                     <br/>
                     {this.props.address}
@@ -29,9 +29,9 @@ class Footer extends React.Component {
                     <a href={'tel:' + this.props.phone}>{this.props.phone}</a>
                     <br/>
                     <a href={'mailto:' + this.props.email}>{this.props.email}</a>
+                    <br/>
+                    Site built by <a href="https://github.com/juliaslohman">Julia Lohman</a>
                 </div>
-
-                Site built by <a href="https://github.com/juliaslohman">Julia Lohman</a>
 
             </footer>
 		);
